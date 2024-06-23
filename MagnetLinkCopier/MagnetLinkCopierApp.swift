647a17a0836561c85b28164c7d45345c7e8905e1
Kenny Do <chinesedewey@gmail.com>
2024-06-22 18:02:48 -0700

    Initial Commit

---
+++ MagnetLinkCopier/MagnetLinkCopierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnetLinkCopierApp.swift
+//  MagnetLinkCopier
+//
+//  Created by Kenny Do on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MagnetLinkCopierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

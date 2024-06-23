051d9ce24177d513d616dec36d2d3946a11ec4da
Kenny Do <chinesedewey@gmail.com>
2024-06-22 19:55:22 -0700

    install pre-commit hooks, make usage instructions look better

--- MagnetLinkCopier/Views/ContentView.swift
+++ MagnetLinkCopier/Views/ContentView.swift
@@ -12,24 +12,30 @@ struct ContentView: View {
         VStack(alignment: .leading) {
             Text("Magnet Link Copier")
                 .font(.title)
-            
+
             GroupBox(label:
-                    Label("Usage Instructions", systemImage: "info.circle")
+                Label("Usage Instructions", systemImage: "info.circle")
             ) {
-                Text("""
-                Magnet links will automatically open in this app.
-                    
-                Steps:
-                1. Tap a link that starts with "magnet://"
-                2. This will show actions you can take
-                3. Copy the link to use in another app
-                """)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("Magnet links will automatically open in this app.")
+
+                VStack(alignment: .leading) {
+                    Text("Steps")
+                        .font(.headline)
+                        .fontWeight(/*@START_MENU_TOKEN@*/ .bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/)
+
+                    Text("1. Tap a link that starts with \"magnet://\"")
+                        .padding(5)
+                    Text("2. This app will open and show actions you can take with the link")
+                        .padding(5)
+                    Text("3. Copy the link to use it in another app")
+                        .padding(5)
+                }.padding(.top, 15)
             }
-            
+
             Spacer()
         }
-            .padding()
+        .padding()
     }
 }
 
